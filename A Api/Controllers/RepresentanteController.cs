d0282526f21741d179dcf0fa1124f2fbf443a591
Alyne <alyn.remo@gmail.com>
2023-10-27 22:34:27 -0500

    Update

--- A Api/Controllers/RepresentanteController.cs
+++ A Api/Controllers/RepresentanteController.cs
@@ -34,7 +34,50 @@ public class RepresentanteController: ControllerBase
         var response = _mapper.Map<Representante, RepresentanteResponse>(representante);
         return Ok(response);
     }
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] RepresentanteRequest resource)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var representante = _mapper.Map<RepresentanteRequest, Representante>(resource);
+            await _representanteDomain.SaveAsync(representante);
+            
+            return Ok(resource);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500);
+        }
+    }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, [FromBody] RepresentanteRequest resource)
+    {
+     
+        var activity = _mapper.Map<RepresentanteRequest, Representante>(resource);
+
+        var result = await _representanteDomain.UpdateAsync(id, activity);
+
+        var activityResource = _mapper.Map<Representante, RepresentanteResponse>(result);
+
+        return Ok(activityResource);
+    }
     
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _representanteDomain.DeleteAsync(id);
+        
+        var activityResource = _mapper.Map<Representante, RepresentanteResponse>(result);
+
+        return Ok(activityResource);
+    }
+
+
 
 
 }
