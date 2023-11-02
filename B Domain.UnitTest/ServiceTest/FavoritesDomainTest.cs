56e82a04750e915bc7e1518949d049c2febd3519
Ricardo Jesus Maguiña Corzo <ricardomaguina29@gmail.com>
2023-11-02 15:03:17 -0500

    UnitTest Favorites

---
+++ B Domain.UnitTest/ServiceTest/FavoritesDomainTest.cs
@@ -0,0 +1,149 @@
+﻿using Data.Model;
+using Data.Persistencia.Impl;
+using Domain.Service;
+using NSubstitute;
+
+namespace B_Domain.UnitTest.ServiceTest;
+
+public class FavoritesDomainTest
+{
+    [Fact]
+    public async Task GetById_WhenFavoritesExists_ShouldReturnFavorites()
+    {
+        // Arrange
+        var favoritesDataMock = Substitute.For<IFavoritesData>();
+        var domain = new FavoritesDomain(favoritesDataMock);
+        var fakeFavorites = new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1 };
+
+        favoritesDataMock.GetById(1).Returns(fakeFavorites);
+
+        // Act
+        var result = await domain.GetById(1);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(fakeFavorites.Id, result.Id);
+        Assert.Equal(fakeFavorites.TouristId, result.TouristId);
+        Assert.Equal(fakeFavorites.ActivitiesId, result.ActivitiesId);
+    }
+
+    [Fact]
+    public async Task GetById_WhenFavoritesDoesNotExist_ShouldThrowKeyNotFoundException()
+    {
+        // Arrange
+        var favoritesDataMock = Substitute.For<IFavoritesData>();
+        var domain = new FavoritesDomain(favoritesDataMock);
+        favoritesDataMock.GetById(1).Returns((Favorites)null);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<KeyNotFoundException>(async () => await domain.GetById(1));
+    }
+
+    [Fact]
+    public async Task GetAll_ShouldReturnListOfFavorites()
+    {
+        // Arrange
+        var favoritesDataMock = Substitute.For<IFavoritesData>();
+        var domain = new FavoritesDomain(favoritesDataMock);
+        var fakeFavoritesList = new List<Favorites>
+        {
+            new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1 },
+            new Favorites { Id = 2, TouristId = 2, ActivitiesId = 2 }
+        };
+
+        favoritesDataMock.GetAll().Returns(fakeFavoritesList);
+
+        // Act
+        var result = await domain.GetAll();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(fakeFavoritesList.Count, result.Count);
+    }
+
+    [Fact]
+    public async Task Create_WithValidFavorites_ShouldReturnTrue()
+    {
+        // Arrange
+        var favoritesDataMock = Substitute.For<IFavoritesData>();
+        var domain = new FavoritesDomain(favoritesDataMock);
+        var validFavorites = new Favorites { TouristId = 1, ActivitiesId = 1 };
+
+        favoritesDataMock.Create(validFavorites).Returns(true);
+
+        // Act
+        var result = await domain.Create(validFavorites);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task Update_WithValidFavorites_ShouldReturnTrue()
+    {
+        // Arrange
+        var favoritesDataMock = Substitute.For<IFavoritesData>();
+        var domain = new FavoritesDomain(favoritesDataMock);
+        var validFavorites = new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1 };
+
+        favoritesDataMock.GetById(1).Returns(validFavorites);
+        favoritesDataMock.Update(validFavorites).Returns(true);
+
+        // Act
+        var result = await domain.Update(validFavorites, 1);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task Update_WithNonExistingFavorites_ShouldReturnFalse()
+    {
+        // Arrange
+        var favoritesDataMock = Substitute.For<IFavoritesData>();
+        var domain = new FavoritesDomain(favoritesDataMock);
+        var invalidFavorites = new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1 };
+
+        favoritesDataMock.GetById(1).Returns((Favorites)null);
+
+        // Act
+        var result = await domain.Update(invalidFavorites, 1);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task Delete_WhenFavoritesExists_ShouldReturnTrue()
+    {
+        // Arrange
+        var favoritesDataMock = Substitute.For<IFavoritesData>();
+        var domain = new FavoritesDomain(favoritesDataMock);
+        var fakeFavorites = new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1 };
+
+        favoritesDataMock.GetById(1).Returns(fakeFavorites);
+        favoritesDataMock.Delete(1).Returns(true);
+
+        // Act
+        var result = await domain.Delete(1);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task Delete_WhenFavoritesDoesNotExist_ShouldReturnFalse()
+    {
+        // Arrange
+        var favoritesDataMock = Substitute.For<IFavoritesData>();
+        var domain = new FavoritesDomain(favoritesDataMock);
+
+        favoritesDataMock.GetById(1).Returns((Favorites)null);
+
+        // Act
+        var result = await domain.Delete(1);
+
+        // Assert
+        Assert.False(result);
+    }
+}
---
+++ C Data.UnitTest/PersistenciaTest/FavoritesDataTest.cs
@@ -0,0 +1,197 @@
+﻿using Data.Context;
+using Data.Model;
+using Data.Persistencia;
+using Microsoft.EntityFrameworkCore;
+
+namespace C_Data.UnitTest.PersistenciaTest;
+
+public class FavoritesDataTest
+{
+    private DbContextOptions<AppDbContext> GetDbOptions(string dbName)
+    {
+        return new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: dbName)
+            .Options;
+    }
+
+    [Fact]
+    public async Task GetById_WhenFavoritesExists_ShouldReturnFavorites()
+    {
+        // Arrange
+        var options = GetDbOptions("GetById_DB");
+        using (var context = new AppDbContext(options))
+        {
+            var favoritesData = new FavoritesData(context);
+            var fakeFavorites = new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1, IsActive = true };
+
+            await context.Favorites.AddAsync(fakeFavorites);
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await favoritesData.GetById(fakeFavorites.Id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(fakeFavorites.Id, result.Id);
+            Assert.Equal(fakeFavorites.TouristId, result.TouristId);
+            Assert.Equal(fakeFavorites.ActivitiesId, result.ActivitiesId);
+        }
+    }
+
+    [Fact]
+    public async Task GetById_WhenFavoritesDoesNotExist_ShouldReturnNull()
+    {
+        // Arrange
+        var options = GetDbOptions("GetById_DB_NotExist");
+        using (var context = new AppDbContext(options))
+        {
+            var favoritesData = new FavoritesData(context);
+            var fakeFavorites = new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1, IsActive = true };
+
+            // Act
+            var result = await favoritesData.GetById(fakeFavorites.Id);
+
+            // Assert
+            Assert.Null(result);
+        }
+    }
+
+    [Fact]
+    public async Task GetAll_WhenFavoritesExist_ShouldReturnListOfFavorites()
+    {
+        // Arrange
+        var options = GetDbOptions("GetAll_DB");
+        using (var context = new AppDbContext(options))
+        {
+            var favoritesData = new FavoritesData(context);
+            var fakeFavorites1 = new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1, IsActive = true };
+            var fakeFavorites2 = new Favorites { Id = 2, TouristId = 2, ActivitiesId = 2, IsActive = true };
+
+            await context.Favorites.AddRangeAsync(fakeFavorites1, fakeFavorites2);
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await favoritesData.GetAll();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+        }
+    }
+
+    [Fact]
+    public async Task GetAll_WhenNoFavoritesExist_ShouldReturnEmptyList()
+    {
+        // Arrange
+        var options = GetDbOptions("GetAll_DB_NoFavorites");
+        using (var context = new AppDbContext(options))
+        {
+            var favoritesData = new FavoritesData(context);
+
+            // Act
+            var result = await favoritesData.GetAll();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+    }
+
+    [Fact]
+    public async Task Create_ShouldReturnTrue()
+    {
+        // Arrange
+        var options = GetDbOptions("Create_DB");
+        using (var context = new AppDbContext(options))
+        {
+            var favoritesData = new FavoritesData(context);
+            var newFavorites = new Favorites { TouristId = 1, ActivitiesId = 1, IsActive = true };
+
+            // Act
+            var result = await favoritesData.Create(newFavorites);
+
+            // Assert
+            Assert.True(result);
+            Assert.NotEqual(0, newFavorites.Id);
+        }
+    }
+
+    [Fact]
+    public async Task Update_WhenFavoritesExists_ShouldReturnTrue()
+    {
+        // Arrange
+        var options = GetDbOptions("Update_DB");
+        using (var context = new AppDbContext(options))
+        {
+            var favoritesData = new FavoritesData(context);
+            var fakeFavorites = new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1, IsActive = true };
+
+            await context.Favorites.AddAsync(fakeFavorites);
+            await context.SaveChangesAsync();
+
+            var updatedFavorites = new Favorites { Id = fakeFavorites.Id, TouristId = 2, ActivitiesId = 2, IsActive = true };
+
+            // Act
+            var result = await favoritesData.Update(updatedFavorites);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+
+    [Fact]
+    public async Task Update_WhenFavoritesDoesNotExist_ShouldReturnFalse()
+    {
+        // Arrange
+        var options = GetDbOptions("Update_DB_NotExist");
+        using (var context = new AppDbContext(options))
+        {
+            var favoritesData = new FavoritesData(context);
+            var updatedFavorites = new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1, IsActive = true };
+
+            // Act
+            var result = await favoritesData.Update(updatedFavorites);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+
+    [Fact]
+    public async Task Delete_WhenFavoritesExists_ShouldReturnTrue()
+    {
+        // Arrange
+        var options = GetDbOptions("Delete_DB");
+        using (var context = new AppDbContext(options))
+        {
+            var favoritesData = new FavoritesData(context);
+            var fakeFavorites = new Favorites { Id = 1, TouristId = 1, ActivitiesId = 1, IsActive = true };
+
+            await context.Favorites.AddAsync(fakeFavorites);
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await favoritesData.Delete(fakeFavorites.Id);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+
+    [Fact]
+    public async Task Delete_WhenFavoritesDoesNotExist_ShouldReturnFalse()
+    {
+        // Arrange
+        var options = GetDbOptions("Delete_DB_NotExist");
+        using (var context = new AppDbContext(options))
+        {
+            var favoritesData = new FavoritesData(context);
+
+            // Act
+            var result = await favoritesData.Delete(1);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
