29389cdf0e4fb9060eee9a2f7fba0c6145b0ea46
OnofreR <carlosonofreruiz16@gmail.com>
2023-11-16 01:32:10 -0500

    comment aggregation

---
+++ B Domain/Service/Impl/ICommentDomain.cs
@@ -0,0 +1,13 @@
+﻿using Data.Model;
+
+namespace Domain.Service.Impl;
+
+public interface ICommentDomain
+{
+    public Task<Comment> GetById(int id);
+    public Task<List<Comment>> GetByContent(Comment content);
+    public Task<List<Comment>> GetAll();
+    public Task<bool> Create(Comment comment);
+    public Task<bool> Update(Comment comment, int id);
+    public Task<bool> Delete(int id);
+}
